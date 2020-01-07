6b4686106671e4a1af54a1338689c10f2b23d913
Ilari Tommiska <ilari97@hotmail.com>
2020-01-07 06:16:48 +0200

    Added procfile

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-const port = process.env.PORT | 3001
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
