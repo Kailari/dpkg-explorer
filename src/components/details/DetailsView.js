2797728c359cb91de0bb99db2430a279ce62e44e
Ilari Tommiska <ilari97@hotmail.com>
2020-01-12 13:02:06 +0200

    Fixed rendering for packages without description or dependencies

--- src/components/details/DetailsView.js
+++ src/components/details/DetailsView.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const DetailsView = ({ changePage, pkg }) => {
+  const parseDependency = dependency => {
+    let bracketIndex = dependency.indexOf('(')
+    let endIndex = bracketIndex > -1 ? bracketIndex : dependency.length
+    return dependency
+      .substring(0, endIndex)
+      .trim()
+  }
+
   return <div>
     <h2>{pkg.Package}</h2>
     <h3>Description</h3>
-    <p>{pkg.Description
+    <p>{(pkg.Description || 'Description not available.')
       .split('\n')
       .map((line, index) => <React.Fragment key={index}>
         <span>{line}</span> <br />
@@ -12,9 +20,9 @@ const DetailsView = ({ changePage, pkg }) => {
     }</p>
 
     <h4>Dependencies</h4>
-    <ul>{pkg.Depends
+    <ul>{(pkg.Depends || 'No dependencies')
       .split(',')
-      .map(dependency => dependency.substring(0, dependency.indexOf('(')).trim())
+      .map(parseDependency)
       .filter((value, index, self) => self.indexOf(value) === index)
       .map((dependency, index) => <li key={index}>{dependency}</li>)
     }</ul>
